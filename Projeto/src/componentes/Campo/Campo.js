5f0c695946c1a921a3269e30f434bf060167eb89
yoneshinzato <chibariyo@gmail.com>
2018-10-25 21:52:56 -0300

    alterações

--- Projeto/src/componentes/Campo/Campo.js
+++ Projeto/src/componentes/Campo/Campo.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import './Campo.css'
 
+class Campo extends React.Component {
+  //herda tudo o que de classes o react tem
+  constructor(props) {
+    super(props)
+    this.state = {
+      erro: ''
+    }
+  }
 
-function Campo(props) {
-  return (
-    <input 
-      id={props.id}
-      className="campo"
-      type={props.type}
-      name={props.name}
-      placeholder={props.placeholder}
-    />
-  )
+  render(){
+    return (
+      <input 
+        id={props.id}
+        className="campo"
+        type={this.props.type}
+        name={this.props.name}
+        placeholder={this.props.placeholder}
+      />
+    )
+  }
 }
+
+
+
 
 export default Campo
