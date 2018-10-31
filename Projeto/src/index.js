83f170b3e218302c997cfd953344c61f510b05e6
yoneshinzato <chibariyo@gmail.com>
2018-10-30 22:23:45 -0300

    contato e quem somos router

--- Projeto/src/index.js
+++ Projeto/src/index.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Conta from './paginas/Conta/Conta';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+//esse componente import o react router, que é uma biblioteca o Switch significa mudar, literalmente
+//associação de uma url com um componente se chama ROTA
+import Conta from './paginas/Conta/Conta'
+import Login from './paginas/Login/Login'
+import QuemSomos from './paginas/QuemSomos/QuemSomos'
+import Contato from './paginas/Contato/Contato'
+
+
 import './index.css'
 
+function App(){
+    return(
+        //HTML dentro da div abaixo tem que aparecer o esqueleto da página e as coisas que vão mudar
+        <div className="app">
+            {/* <NavBar /> */}
+            <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path="/login" component={Login} />
+                <Route path="/conta" component={Conta} />
+                <Route path="/quem-somos" component={QuemSomos} />
+                <Route path="/contato" component={Contato} />
+                
+            </Switch>
+            
+        </div>
+        //atributo path é string, por isso tá entre aspas
+        //path="/" component={X} é como se fosse para deixar essa página X na home
+    )
+}
 
-const pagina = <Conta />
-//alo alo WBrasil
 
-const divisaoProjeto = document.getElementById('projeto')
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('projeto')
+    )
 
-ReactDOM.render(pagina, divisaoProjeto)
+// BrowserRouter escuta oa mudanças de url da página toda 
+//para escolher o componente certo para mostrar na tela
---
+++ Projeto/src/paginas/Contato/Contato.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Contato.css'
+
+
+function Contato(){
+    return(
+        <main className="quem-somos">
+            <h1>Quem somos</h1>
+            <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
+            </p>
+        </main>
+    )
+}
+
+export default Contato
