95f538efb738d249e81f97b7e5c3d5d3e1abb1a7
yoneshinzato <chibariyo@gmail.com>
2018-12-03 18:47:27 -0200

    lista portits

--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,77 +1,96 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 //importa o conect
-import { Redirect } from 'react-router-dom'
-import './Home.css'
-import Postit from '../../componentes/Postit/Postit'
-
-import carregando from './carregando.svg'
+import { Redirect } from "react-router-dom";
+import "./Home.css";
+import Postit from "../../componentes/Postit/Postit";
+import { listaPostits } from "../../redux/actions";
+import carregando from "./carregando.svg";
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            carregando: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      carregando: true
+    };
+  }
+  //pode deixar o state inicial do carregando como true
+
+  //vai chamar a lista de postits
+  componentDidMount() {
+    //busca da api a lista de postits e põe no state da store
+    //só é chamado uma vez
+    //método vai ser chamado pelo React toda vez que o comp. Home
+    //aparecer na tela pela primeira vez, que vai disparar a função listaPostits()
+    this.props.listaPostits();
+  }
+
+  componentDidUpdate() {
+    //vai ser chamado pelo React toda vez que a tela atualizar ou alterar ou acrescentar algo, vai ser chamado e atualizar
+    if (this.state.carregando) {
+      this.setState({ carregando: false });
+    }
+  }
+
+  render() {
+    if (!this.props.usuario) {
+      return <Redirect to="/login" />;
+      //vai importar do react router
+      //tá usando classe então tem que usar this.props.usuario
     }
 
-    render() {
-        if (!this.props.usuario) {
-            return <Redirect to="/login" />
-            //vai importar do react router 
-            //tá usando classe então tem que usar this.props.usuario
-        }
+    return (
+      <main className="home">
+        {this.state.carregando ? (
+          <img className="home__loading" src={carregando} alt="carregando" />
+        ) : (
+          <div>
+            <Postit />
+            {/* esse é o postit default de cadastro, por isso tá vazio */}
 
-        return (
-            <main className="home">
-                {this.state.carregando ? (
-                    <img className="home__loading" src={carregando} alt="carregando" />
-                ) : (
-                        <div>
-                            <Postit />
-                            {/* esse é o postit default de cadastro, por isso tá vazio */}
-
-
-                            <div>
-                                {/* percorrer toda lista e para cada postits vai usar uma tag postit */}
-                                {this.props.postits.map(postit => (
-                                    // para cada item da lista retorna uma tag postit
-                                    //pega o título e o testo do postit e passar
-                                    <Postit
-                                        key={postit.id} //o key é específico do react
-        
-                                        //precisa desse identificador para cada postit
-                                        //e pra parar de aparecer um erro
-                                        id={postit.id}
-                                        titulo={postit.titulo}
-                                        texto={postit.texto} //são novas propriedades para o postit
-                                    //entao vai ter que fazer o título e o texto aparecerem dentro do input e do textarea
-                                    // tudo que vai como atributo acima como props passa como props em postit.js
-                                    />
-                                ))}
-                            </div>
-
-                        </div>
-
-                    )
-
-                }
-            </main>
-            //if dentro da tag coloca chave
-        )
-    }
-
+            <div>
+              {/* percorrer toda lista e para cada postits vai usar uma tag postit */}
+              {this.props.postits.map(postit => (
+                // para cada item da lista retorna uma tag postit
+                //pega o título e o testo do postit e passar
+                <Postit
+                  key={postit.id} //o key é específico do react
+                  //precisa desse identificador para cada postit
+                  //e pra parar de aparecer um erro
+                  id={postit.id}
+                  titulo={postit.titulo}
+                  texto={postit.texto} //são novas propriedades para o postit
+                  //entao vai ter que fazer o título e o texto aparecerem dentro do input e do textarea
+                  // tudo que vai como atributo acima como props passa como props em postit.js
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </main>
+      //if dentro da tag coloca chave
+    );
+  }
 }
 
-
 export default connect(
-    (state) => ({ usuario: state.usuario, postits: state.postits }))(Home)
+  state => (
+    {
+      usuario: state.usuario,
+      postits: state.postits
+    },
+    { listaPostits }
+  )
+)(Home);
 
 // const props = {
-//     usuario: state.usuario,
-//     postits: state.postits
-// }
+//   usuario: state.usuario,
+//   postits: state.postits,
+//   listaPostits
+// };
 
-{/* <Home usuario={state.usuario} postits={state.postits} /> */ }
-
-
+// <Home
+//   usuario={state.usuario}
+//   postits={state.postits}
+//   listaPostits={listaPostits}
+// />;
--- Projeto/src/redux/reducers.js
+++ Projeto/src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux' //isso é prório do reducers. 
+import { combineReducers } from "redux"; //isso é prório do reducers.
 //usa essa porque combina mais de um reducers
 
 // criar uma função pra cuidar dos dados do usuário (redutor de usuário)
@@ -7,46 +7,48 @@ import { combineReducers } from 'redux' //isso é prório do reducers.
 //usa switch case porque são varias coisas que serão verificadas
 
 //fora da função precisa criar uma variável para indicar o estado inicial do usuário
-let usuarioInicial = null  //mudou para let . antes era const
+let usuarioInicial = null; //mudou para let . antes era const
 //sempre tem que receber um estado inicial
 
-const json = localStorage.getItem('usuario')
-if(json){
-    usuarioInicial = JSON.parse(json)
+const json = localStorage.getItem("usuario");
+if (json) {
+  usuarioInicial = JSON.parse(json);
 }
 //caso o state não receba nenhum valor, venha parametro vazio, vai receber o valor inicial do usuarioInicial
 
-function usuario (usuarioAtual = usuarioInicial, action) {
-    switch(action.type) {
-        //verifica o tipo da ação
-        //e usar cada case em caixa alta é uma convenção
+function usuario(usuarioAtual = usuarioInicial, action) {
+  switch (action.type) {
+    //verifica o tipo da ação
+    //e usar cada case em caixa alta é uma convenção
 
-        case 'LOGA_USUARIO':
-        //tem que receber um objeto que representa o usuário logado
-        //é da ação que pega os dados do usuário. a ação é o parametro
-        const usuarioLogado = action.dados
+    case "LOGA_USUARIO":
+      //tem que receber um objeto que representa o usuário logado
+      //é da ação que pega os dados do usuário. a ação é o parametro
+      const usuarioLogado = action.dados;
 
-        const json = JSON.stringify(usuarioLogado)
-        //tornar texto e guarda na variável
-        //guarda em local storage
-        localStorage.setItem('usuario',json)
+      const json = JSON.stringify(usuarioLogado);
+      //tornar texto e guarda na variável
+      //guarda em local storage
+      localStorage.setItem("usuario", json);
 
-        //dados vem da action que mostra dados tipo email e o que mais foi passado
-        return usuarioLogado //com os dados
-        case 'DESLOGA_USUARIO':
-        //para remover a locaStorage
-        localStorage.removeItem('usuario')
-        const usuarioDeslogado = null
-        //null porque acabou de sair do sistema
-        return usuarioDeslogado //retorna vazio, nulo
-        default:
-        return usuarioAtual 
-        //state com o estado inicial
-        //caso não logue, nem deslogue, o default vai ser  com o estado atual usuarioAtual
-        //retorna o parametro do jeito que recebeu
-        //qd recebe postit o return é default
+      //dados vem da action que mostra dados tipo email e o que mais foi passado
+      return usuarioLogado; //com os dados
+    case "DESLOGA_USUARIO":
+      //para remover a locaStorage
+      localStorage.removeItem("usuario");
+      const usuarioDeslogado = null;
+      //null porque acabou de sair do sistema
+      return usuarioDeslogado; //retorna vazio, nulo
 
-    }
+    case "LISTA_POSTITS":
+      return action.dados;
+    default:
+      return usuarioAtual;
+    //state com o estado inicial
+    //caso não logue, nem deslogue, o default vai ser  com o estado atual usuarioAtual
+    //retorna o parametro do jeito que recebeu
+    //qd recebe postit o return é default
+  }
 }
 
 //sempre retornar novo objeto porque o redux preza pela imutabilidade
@@ -55,39 +57,41 @@ function usuario (usuarioAtual = usuarioInicial, action) {
 //     dados: {email: 'chibaeriyo@gmail.com'}
 // }
 
-function postits(stateAtualDoPostit = [], action){
-    //também é um redutor, e tb recebe switch case, igual acima
-    switch(action.type) {
-        //verifica o tipo da ação
-        //e usar cada case em caixa alta é uma convenção
-        //só faz algo quando receber postits novos portanto, a logausuario nao vai fazer diferença]
-        case 'CADASTRA_POSTIT':
-            //retorna um novo array contendo o postit - o concat
-            return stateAtualDoPostit.concat(action.dados)
-            //para acessar todos os dados do postit que sao título e a mensagem
-            //vai concatenar o state com os dados e retornar um novo array
-        case 'ALTERA_POSTIT':
-        return stateAtualDoPostit.map(item=>
-            item.id=== action.dados.id ? action.dados : item)
-        //se o postit tem igual id igual ao id que tem os dados iguais dos dados da ação troca os dados, senão retorna como estava
-        //vai retornar uma array com os dados alterados
-        case 'REMOVE_POSTIT':
-        return stateAtualDoPostit.filter(item =>
-            item.id !== action.id)
-        //filtra os com os ids diferentes porque a que tem o id que eu quero remover não interessa mais 
-        //vai retornar um nov array sem o que remove
-        //filtrar os posts que tenham um id diferente do que vai remover
-        default: 
-        return stateAtualDoPostit
-    }
+function postits(stateAtualDoPostit = [], action) {
+  //também é um redutor, e tb recebe switch case, igual acima
+  switch (action.type) {
+    //verifica o tipo da ação
+    //e usar cada case em caixa alta é uma convenção
+    //só faz algo quando receber postits novos portanto, a logausuario nao vai fazer diferença]
+    case "CADASTRA_POSTIT":
+      //retorna um novo array contendo o postit - o concat
+      return stateAtualDoPostit.concat(action.dados);
+    //para acessar todos os dados do postit que sao título e a mensagem
+    //vai concatenar o state com os dados e retornar um novo array
+    case "ALTERA_POSTIT":
+      return stateAtualDoPostit.map(item =>
+        item.id === action.dados.id ? action.dados : item
+      );
+    //se o postit tem igual id igual ao id que tem os dados iguais dos dados da ação troca os dados, senão retorna como estava
+    //vai retornar uma array com os dados alterados
+    case "REMOVE_POSTIT":
+      return stateAtualDoPostit.filter(item => item.id !== action.id);
+    //filtra os com os ids diferentes porque a que tem o id que eu quero remover não interessa mais
+    //vai retornar um nov array sem o que remove
+    //filtrar os posts que tenham um id diferente do que vai remover
+    case "LISTA_POSTITS":
+      return action.dados;
+    default:
+      return stateAtualDoPostit;
+  }
 }
 
 // const objeto = {usuario:usuario, postits: postits}
 
 const reducers = combineReducers({
-    usuario: usuario,
-    postits: postits
-})
+  usuario: usuario,
+  postits: postits
+});
 //aqui tem que receber as duas funções combinadas
 
-export default reducers //e por fim eles são exportados
+export default reducers; //e por fim eles são exportados
